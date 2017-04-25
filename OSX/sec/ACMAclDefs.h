a1d48293b895ea7ac947ade151c5011b00c4706e
Andrew Hyatt <ahyattdev@icloud.com>
2017-04-24 17:41:44 -0700

    Get existing targets to build Fix some commented out source files

---
+++ OSX/sec/ACMAclDefs.h
@@ -0,0 +1,17 @@
+#ifndef _ACMACLDEFS_H_
+#define _ACMACLDEFS_H_
+
+// Someone please reverse-engineer this,
+// these are guessed and not verified
+#define kAKSKeyAcl "AKSKeyAcl"
+#define kAKSKeyOpDefaultAcl "AKSKeyOpDefaultAcl"
+#define kACMKeyAclConstraintUserPasscode "ACMKeyAclConstraintUserPasscode"
+#define kACMKeyAclParamBioCatacombUUID "ACMKeyAclParamBioCatacombUUID"
+#define kACMKeyAclConstraintBio "ACMKeyAclConstraintBio"
+#define kACMKeyAclParamBioCatacombUUID "ACMKeyAclParamBioCatacombUUID"
+#define kACMKeyAclParamBioDatabaseHash "ACMKeyAclParamBioDatabaseHash"
+#define kACMKeyAclParamKofN "ACMKeyAclParamKofN"
+#define kACMKeyAclConstraintKofN "ACMKeyAclConstraintKofN"
+#define kAKSKeyAclParamRequirePasscode "AKSKeyAclParamRequirePasscode"
+
+#endif
---
+++ OSX/sec/ACMDefs.h
@@ -0,0 +1,13 @@
+#ifndef _ACMDEFS_H_
+#define _ACMDEFS_H_
+
+// Someone please reverse-engineer this,
+// these are guessed and not verified
+#define kAKSKeyOpDecrypt "AKSKeyOpDecrypt"
+#define kAKSKeyOpEncrypt "AKSKeyOpEncrypt"
+#define kAKSKeyOpDelete "AKSKeyOpDelete"
+#define kACMPolicyDeviceOwnerAuthentication "ACMPolicyDeviceOwnerAuthentication"
+#define kACMKeyAclConstraintPolicy "ACMKeyAclConstraintPolicy"
+#define kAKSKeyOpSync "AKSKeyOpSync"
+
+#endif
