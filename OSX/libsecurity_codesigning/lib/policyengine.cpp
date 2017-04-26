43572f50e8eb034ea77745f527b93a9c96556379
Andrew Hyatt <ahyattdev@icloud.com>
2017-04-25 18:09:08 -0700

    Get more missing parts to build

--- OSX/libsecurity_codesigning/lib/policyengine.cpp
+++ OSX/libsecurity_codesigning/lib/policyengine.cpp
@@ -46,6 +46,7 @@
 
 extern "C" {
 #include <OpenScriptingUtilPriv.h>
+#include <CoreFoundation/CFURLPriv.h>
 }
 
 
---
+++ include/unknown.h
@@ -0,0 +1,15 @@
+#ifndef _UNKNOWN_H_
+#define _UNKNOWN_H_
+
+// Assumed
+#define kLSDownloadRiskCategoryKey CFSTR("LSDownloadRiskCategory")
+
+#define kLSRiskCategorySafe "LSRiskCategorySafe"
+#define kLSRiskCategoryNeutral "LSRiskCategoryNeutral"
+#define kLSRiskCategoryUnknown "LSRiskCategoryUnknown"
+#define kLSRiskCategoryMayContainUnsafeExecutable "LSRiskCategoryMayContainUnsafeExecutable"
+
+// Also assumed
+#define QTN_FLAG_ASSESSMENT_OK 1
+
+#endif
